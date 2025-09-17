a304e90ee036959b7bfe940f3aa981f5fb0f7228
Byter8113 <bogdanaminaa3@gmail.com>
2025-09-17 14:05:58 +0300

    Added class Book with encapsulation

---
+++ src/com/example/library/model/Book.java
@@ -0,0 +1,48 @@
+package com.example.library.model;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private boolean available;
+    
+    Book(int id, String title, String author, boolean available) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.available = available;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book: " + id + "\ntitle: " + title + "\nauthor: " + author + "\navailable: " + available;
+    }
+}
