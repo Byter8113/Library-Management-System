21a151dae2c3533fe8e2626f26a9048a0f3f1b89
Byter8113 <bogdanaminaa3@gmail.com>
2025-09-30 12:44:16 +0300

    Added BookController class and create two books and print all books

--- src/com/example/library/LibraryApplication.java
+++ src/com/example/library/LibraryApplication.java
@@ -1,7 +1,14 @@
 package com.example.library;
 
+import com.example.library.controller.BookController;
+
 public class LibraryApplication {
     public static void main(String[] args) {
+        BookController controller = new BookController();
+
+        controller.addBook("How learn Java?", "Bobby");
+        controller.addBook("Why learn Java so hard?", "Crying Bobby");
         
+        controller.listBooks();
     }
 }
