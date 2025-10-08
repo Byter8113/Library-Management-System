2440cb077bcff221c9dd2ae56239256c204c581f
Byter8113 <bogdanaminaa3@gmail.com>
2025-10-08 16:20:38 +0300

    Created class Magazine and update BookService and BookRepository classes

---
+++ src/com/example/library/model/Magazine.java
@@ -0,0 +1,17 @@
+package com.example.library.model;
+
+public class Magazine extends LibraryItem {
+    private String publisher;
+    private int issueNumber;
+
+    public Magazine(long id, String title, boolean available, String publisher, int issueNumber) {
+        super(id, title, available);
+        this.publisher = publisher;
+        this.issueNumber = issueNumber;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Magazine: " + getTitle() + " (Issue " + issueNumber + ") by " + publisher;
+    }
+}
