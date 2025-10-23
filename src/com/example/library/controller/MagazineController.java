98cb28fc5a36a73f60cd90113ca2416a0e4d537b
Byter8113 <bogdanaminaa3@gmail.com>
2025-10-23 14:35:04 +0300

    Changed ConsoleView class

--- src/com/example/library/controller/MagazineController.java
+++ src/com/example/library/controller/MagazineController.java
@@ -6,8 +6,8 @@ import com.example.library.service.MagazineServiceImpl;
 
 public class MagazineController {
     private MagazineServiceImpl service = new MagazineServiceImpl();
-    public void addMagazine(String title, boolean available, String publisher, int issueNumber) {
-        service.addMagazine(title, available, publisher, issueNumber);
+    public void addMagazine(String title, String publisher, int issueNumber) {
+        service.addMagazine(title, publisher, issueNumber);
     }   
     public List listMagazines() {
         return service.listMagazines();    
--- src/com/example/library/view/ConsoleView.java
+++ src/com/example/library/view/ConsoleView.java
@@ -4,22 +4,78 @@ import java.util.List;
 import java.util.Scanner;
 
 import com.example.library.controller.BookController;
+import com.example.library.controller.EBookController;
+import com.example.library.controller.MagazineController;
 import com.example.library.model.Book;
+import com.example.library.model.Magazine;
 
 public class ConsoleView {
-    private BookController controller = new BookController();
+    private BookController booksController = new BookController();
+    private MagazineController magazinesController = new MagazineController();
+    private EBookController eBooksController = new EBookController();
     Scanner scanner = new Scanner(System.in);
+    // public void start() {
+    //     String promt = null;
+    //     while(true) {
+    //         System.out.println("Choose: \n1. Add book \n2. List all books \n3. Exit");
+    //         promt = scanner.nextLine();
+    //         if (promt.equals("1")) {
+    //             addBookFlow();
+    //         } else if(promt.equals("2")) {
+    //             listBooksFlow();
+    //         } else if(promt.equals("3")) {
+    //             return;
+    //         } else if(promt != null) {
+    //             System.out.println("such a team does not exist");
+    //         }
+    //     }
+    // }
+
     public void start() {
         String promt = null;
         while(true) {
-            System.out.println("Choose: \n1. Add book \n2. List all books \n3. Exit");
+            System.out.println("Choose: \n1. Books \n2. Magazines \n3. EBooks \n4. Exit");
+            promt = scanner.nextLine();
+            if (promt.equals("1")) {
+                books();
+            } else if(promt.equals("2")) {
+                magazines();
+            } else if(promt.equals("3")) {
+                // eBooks();
+            } else if(promt.equals("4")) {
+                System.out.println("such a team does not exist");
+            }
+        }
+    }
+
+    public void books() {
+        String promt = null;
+        while(true) {
+            System.out.println("Choose: \n1. Add book \n2. List all books \n3. Back");
             promt = scanner.nextLine();
             if (promt.equals("1")) {
                 addBookFlow();
             } else if(promt.equals("2")) {
                 listBooksFlow();
             } else if(promt.equals("3")) {
-                return;
+                start();
+            } else if(promt != null) {
+                System.out.println("such a team does not exist");
+            }
+        }
+    }
+
+    public void magazines() {
+        String promt = null;
+        while(true) {
+            System.out.println("Choose: \n1. Add magazine \n2. List all magazines \n3. Back");
+            promt = scanner.nextLine();
+            if (promt.equals("1")) {
+                addMagazineFlow();
+            } else if(promt.equals("2")) {
+                listMagazinesFlow();
+            } else if(promt.equals("3")) {
+                start();
             } else if(promt != null) {
                 System.out.println("such a team does not exist");
             }
@@ -31,12 +87,32 @@ public class ConsoleView {
         String title = scanner.nextLine();
         System.out.println("Enter title: ");
         String author = scanner.nextLine();
-        controller.addBook(title, author);
+        booksController.addBook(title, author);
         System.out.println("Book was added!");
     }
 
+    private void addMagazineFlow() {
+        System.out.println("Enter author: ");
+        String title = scanner.nextLine();
+        System.out.println("Enter publisher: ");
+        String publisher = scanner.nextLine();
+        System.out.println("Enter issueNumber: ");
+        int issueNumber = scanner.nextInt();
+        magazinesController.addMagazine(title, publisher, issueNumber);
+        System.out.println("Magazine was added!");
+    }
+
+    private void listMagazinesFlow() {
+        List<Book> listMagazines = magazinesController.listMagazines();  
+        if (listMagazines.isEmpty()) {
+            System.out.println("No magazi!");
+        } else {
+            System.out.println(listMagazines);
+        }
+    }
+
     private void listBooksFlow() {
-        List<Book> listBooks = controller.listBooks();  
+        List<Book> listBooks = booksController.listBooks();  
         if (listBooks.isEmpty()) {
             System.out.println("No books!");
         } else {
